d062006cc4081e659dd507a3e3efa2e39da3ede8
Loic Tiemani <loictiems@github.com>
2021-01-23 00:59:34 -0500

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,40 +1,59 @@
 // Belly Button Biodiversity - Plotly.js
-
-function readData(sample){
-
-// Use `d3.json` to Fetch the Metadata for a Sample
-    d3.json("samples.json").then(function(data) {
+function buildPlot(sample) {
+    d3.json("samples.json").then(function(data){ 
 
         console.log(data)
+    var ids = data.samples[0].otu_ids;
+    console.log(ids)
+    var sampleValues = data.samples[0].sample_values.slice(0,10).reverse();
+    console.log(sampleValues)
+    //var  otu_ids =data.sample[0]otu_ids;
+    //var  otu_labels = data.otu_labels;
+    
+    
+})
 
-        
-        // use .html("") to clear any existing Data
-        var panel = d3.select("#sample-metadata");
-        panel.html("");
-
-        // Use object.entries to add Each key value pair to the panel
-
-        Object.entries(data).forEach(([key, value]) =>{
-
-        panel.append("h6").text(key,value);
-        //console.log(key, value)
-
-        // use d3 to append new tags for Each-Value in the MetaData
-        })
-        // Bonus: 
-        buildGauge(data.WFREQ);
-    });
-};
-readData();
-
-function buildCharts(sample) {
-
-    d3.json("samples.json").then(function(data) {
-        const otu_ids =data.otu_ids;
-        
-
-
-    }
 }
 
+buildPlot();
+function readData(sample){
 
+    // Use `d3.json` to Fetch the Metadata for a Sample
+        d3.json("samples.json").then(function(data) {
+    
+            //console.log(data)
+    
+            
+            // use .html("") to clear any existing Data
+            var panel = d3.select("#sample-metadata");
+            panel.html("");
+    
+            // Use object.entries to add Each key value pair to the panel
+    
+            Object.entries(data).forEach(([key, value]) =>{
+    
+            panel.append("h6").text(key,value);
+            //console.log(key, value)
+    
+            // use d3 to append new tags for Each-Value in the MetaData
+            })
+            // Bonus: 
+           // buildGauge(data.WFREQ);
+        });
+    };
+    //readData();
+    
+    //function buildCharts(sample) {
+    
+       // d3.json("samples.json").then(function(data) {
+           
+            //const sample_values = data.samples_values;
+           // const otu_ids =data.otu_ids;
+            //const otu_lables = data.otu_labels;
+    
+    
+    
+        //})
+    //}
+    
+    
