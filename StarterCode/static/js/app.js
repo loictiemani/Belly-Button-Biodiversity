8dc4dd0159822f8bc1a66b2d421f27b05d6e5f78
Loic Tiemani <loictiems@github.com>
2021-01-22 22:20:13 -0500

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -8,11 +8,14 @@ d3.json("samples.json").then(function(data) {
     console.log(data)
     // use .html("") to clear any existing Data
     var panel = d3.select("#sample-metadata");
-    panel.hmtl("");
+    panel.html("");
 
     // Use object.entries to add Each key value pair to the panel
 
-    Object.entries(data).forEach(function[Key, value])
+    Object.entries(data).forEach(function([key, value]) {
+
+        console.log(key, value)
+    })
 
 
 
