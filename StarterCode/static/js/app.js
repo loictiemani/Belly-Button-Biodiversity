64938808eb8ca4183f004a1186c92c1921fdfb42
Loic Tiemani <loictiems@github.com>
2021-01-22 21:12:42 -0500

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -6,7 +6,17 @@ function readData(sample){
 d3.json("samples.json").then(function(data) {
 
     console.log(data)
+    // use .html("") to clear any existing Data
+    var panel = d3.select("#sample-metadata");
+    panel.hmtl("");
+    
+
+
+
+
 
 });
 };
 readData();
+
+
