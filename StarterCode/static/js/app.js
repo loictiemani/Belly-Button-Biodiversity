7f3f724c623b2cdbe3d84d237c8929d2501581b7
Loic Tiemani <loictiems@github.com>
2021-01-22 23:21:18 -0500

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,25 +3,26 @@
 function readData(sample){
 
 // Use `d3.json` to Fetch the Metadata for a Sample
-d3.json("samples.json").then(function(data) {
+    d3.json("samples.json").then(function(data) {
 
-    console.log(data)
-    // use .html("") to clear any existing Data
-    var panel = d3.select("#sample-metadata");
-    panel.html("");
+        console.log(data)
 
-    // Use object.entries to add Each key value pair to the panel
+        
+        // use .html("") to clear any existing Data
+        var panel = d3.select("#sample-metadata");
+        panel.html("");
 
-    Object.entries(data).forEach(function([key, value]) {
+        // Use object.entries to add Each key value pair to the panel
 
-        console.log(key, value)
-    })
+        Object.entries(data).forEach(([key, value]) =>{
 
+        panel.append("h6").text(key,value);
+        //console.log(key, value)
 
-
-
-
-});
+        // use d3 to append new tags for Each-Value in the MetaData
+        })
+    // Bnus: 
+    });
 };
 readData();
 
