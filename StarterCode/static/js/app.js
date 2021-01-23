3adc0ec49fdc61c8d32700eda7ba7edf03875875
Loic Tiemani <loictiems@github.com>
2021-01-22 20:46:19 -0500

    Update app.js

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,15 @@
+// Belly Button Biodiversity - Plotly.js
+
+function buildMetadata(sample){
+
+// Use `d3.json` to Fetch the Metadata for a Sample
+d3.json("samples.json").then(function(data) {
+
+    console.log(data)
+
+});
+
+
+
+
+};
